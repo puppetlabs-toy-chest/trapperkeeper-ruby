53c918e539b9a7e190e2c19274b6a3b9d3135cdf
Chris Price <chris@puppetlabs.com>
2013-12-17 15:41:15 -0800

    Add in all initial application code

---
+++ test/clojure/examples/counter/clj_count_service.clj
@@ -0,0 +1,13 @@
+(ns examples.counter.clj-count-service
+  (:require [puppetlabs.trapperkeeper.core :refer [defservice]]
+            [clojure.tools.logging :as log]))
+
+(defservice count-service
+  "This is a simple counter service.  The counter starts at zero, and the service
+  provides one function, `inc-and-get`, which increments the counter and returns
+  the new value"
+  {:depends  []
+   :provides [inc-and-get]}
+  (log/info "############## Counter service starting up.")
+  (let [counter (atom 0)]
+    {:inc-and-get (fn [] (swap! counter inc))}))
---
+++ test/clojure/examples/counter/count_service_java_bridge.clj
@@ -0,0 +1,23 @@
+(ns examples.counter.count-service-java-bridge
+  (:import [examples.counter CountService])
+  (:require [puppetlabs.trapperkeeper.core :refer [defservice]]
+            [clojure.tools.logging :as log]))
+
+(defservice count-service-java-bridge
+  "This service provides a simple java bridge for the clojure count-service.  It
+  accomplishes this via an abstract Java class (test/java/examples/counter/CountService.java)
+  which provides a Java signature for the clojure function.  The Java class also
+  serves as a singleton holder so that we can store an instance of the service,
+  and a getter that allows us to retrieve the singleton instance from other code
+  (such as the JRuby sinatra-consumer web app."
+  {:depends [[:count-service inc-and-get]]
+   :provides []}
+  (log/info "Count service java bridge initializing.")
+  ;; Create an instance of the java CountService class
+  (let [cs (proxy [CountService] []
+              ;; provide an implementation of the abstract method, which simply
+              ;; wraps the clojure function that was injected by trapperkeeper
+              (incAndGet [] (inc-and-get)))]
+    ;; store the singleton instance so that it can be retrieved from ruby.
+    (CountService/setInstance cs))
+  {})
